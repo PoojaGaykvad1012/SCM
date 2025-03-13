68991e8eef843238ac080fb3d62a16d82ca183a7
PoojaGaykvad1012 <poojagaykvad2003@gmail.com>
2025-03-13 11:23:09 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const mongoose= require('mongoose')
+const app = express()
+const port=3000;
+
+
+app.get("/",(req,res)=>
+{
+    console.log("hello Pooja Gaykvad");
+})
+mongoose.connect('mongodb://localhost:27017/Demo2')
+.then(()=>
+console.log('connected')
+)
+.catch(()=>
+console.log('Not Connected')
+)
+const Emplooye= new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    Email:{
+        type:String,
+        required:true,
+        unique:true
+    }
+}
+)
+const Emodel=new mongoose.model('emdata',Emplooye)
+app.listen(port,()=>{
+    console.log(`server is runing on port ${port}`);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
